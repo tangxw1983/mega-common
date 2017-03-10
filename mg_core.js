adaeb9a8b3a19c06c4a93f35abd8e4ac8e583b06
WANJO CHAN <wanjochan@users.noreply.github.com>
2017-03-10 23:32:02 +0800

    Update mg_core.js

--- mg_core.js
+++ mg_core.js
@@ -17,6 +17,7 @@ function argv2o(argv){
 	}
 	return argv_o;
 }
+function sha1(d){var l=0,a=0,f=[],b,c,g,h,p,e,m=[b=1732584193,c=4023233417,~b,~c,3285377520],n=[],k=unescape(encodeURI(d));for(b=k.length;a<=b;)n[a>>2]|=(k.charCodeAt(a)||128)<<8*(3-a++%4);for(n[d=b+8>>2|15]=b<<3;l<=d;l+=16){b=m;for(a=0;80>a;b=[[(e=((k=b[0])<<5|k>>>27)+b[4]+(f[a]=16>a?~~n[l+a]:e<<1|e>>>31)+1518500249)+((c=b[1])&(g=b[2])|~c&(h=b[3])),p=e+(c^g^h)+341275144,e+(c&g|c&h|g&h)+882459459,p+1535694389][0|a++/20]|0,k,c<<30|c>>>2,g,h])e=f[a-3]^f[a-8]^f[a-14]^f[a-16];for(a=5;a;)m[--a]=m[a]+b[a]|0}for(d="";40>a;)d+=(m[a>>3]>>4*(7-a++%8)&15).toString(16);return d}
 function ns(s,r,c,i,k){r=r||/^u/.test(typeof window)?global:window;c=(s||"").split('.');for(i=0;i<c.length;i++){k=c[i];if(!k)break;r[k]||(r[k]={});r=r[k];}return r}
 function date_pattern(fmt,dt){
 	if(!dt) dt=new Date();
@@ -255,8 +256,15 @@ function formatResponse(err,data) {
 
 	return ret;
 }
-
+var ajax={
+	x:function(){return new(self.XMLHttpRequest||ActiveXObject)("Microsoft.XMLHTTP");}
+	,send:function(u,f,m,a){var x=this.x();x.open(m,u,true);x.onerror=function(ex){f({STS:"KO",errmsg:"Network Failed."+ex},-1,u);};x.onreadystatechange=function(){if(x.readyState==4){f(x.responseText,x.status,u);}};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(a);}
+	,get:function(url,func){this.send(url,func,'GET');}
+	,gets:function(url){var x=this.x();x.open('GET',url,false);x.send(null);return x.responseText;}
+	,post:function(url,func,args){this.send(url,func,'POST',args);}
+};
 if(typeof(exports)!="undefined"){
+	exports.ajax=ajax;
 	exports.ns=ns;//namespace
 	exports.console_log=console_log;//wrapper for the console.log
 	exports.argv2o=argv2o;//quick func for convert the argv to o
